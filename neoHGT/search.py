62f7467d3fcf384cf5195131e0276ce1a8a3d433
neoLIZV <126172271+neoLIZV@users.noreply.github.com>
2023-04-25 13:36:31 +0200

    Update search.py

--- neoHGT/search.py
+++ neoHGT/search.py
@@ -2134,7 +2134,7 @@ class Search(object):
 		if extrargs:
 			cmd += ' ' + extrargs
 		ec, out = run_command(cmd, merge=False)
-		if ec:
+		if ec != 0: # RC=0 indicates successful completion
 			raise ValueError(f'diamond failed with error code {ec}.')
 
 		remove(tmpin)
